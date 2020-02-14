566d0cf01a4af0107ffc8fe461bf5db23542c1aa
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-14 09:47:56 -0300

    02.Acesso ao recurso pela API

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,16 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+
 /** @var \Laravel\Lumen\Routing\Router $router */
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => '/api'], function () use ($router) {
-    $router->get('/series', 'SeriesController@index');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('series', 'SeriesController@index');
+    $router->post('series', 'SeriesController@store');
+    $router->get('series/{id}', 'SeriesController@show');
+    $router->put('series/{id}', 'SeriesController@update');
+    $router->delete('series/{id}', 'SeriesController@destroy');
 });
